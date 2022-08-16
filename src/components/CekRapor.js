cfedf665ddcc5e123a7ae4828d91242b5cdd54d2
ComRV <r.duika.9d@gmail.com>
2022-08-16 18:22:11 +0700

    fix responsive

--- src/components/CekRapor.js
+++ src/components/CekRapor.js
@@ -10,11 +10,11 @@ const CekRapor = () => {
             <div className="flex flex-col m-auto">
                 <img src={pendidikan} alt="tut wuri handayani" className="w-1/4 m-auto mt-3 tablet:w-40" />
                 <h1 className="text-center font-lato font-semibold text-2xl">Sekolah Dasar Internasional Bangka</h1>
-                <h1 className="text-center mt-10 mb-2 font-nunito-sans font-bold text-slate-900 text-xl">Cek Rapor anda dibawah ini</h1>
+                <h1 className="text-center mt-10 mb-2 font-lato font-bold text-slate-900 text-xl">Cek Rapor anda dibawah ini</h1>
                 <form>
                     <div className="flex flex-col">
-                        <input type="number" className="w-11/12 h-9 border-2 pl-3 border-slate-200 bg-gray-50 rounded-md m-auto focus:outline-blue-500 drop-shadow-lg tablet:w-5/6 laptop:w-2/4" placeholder="Masukkan nomor NISN anda disini" />
-                        <button className="font-lato w-11/12 h-9 m-auto mt-3 rounded-md bg-blue-500 hover:bg-blue-600 active:bg-blue-700 duration-150 tablet:w-5/6  laptop:w-2/4">Cek NISN</button>
+                        <input type="number" className="w-9/12 h-9 border-2 pl-3 border-slate-200 bg-gray-50 rounded-md m-auto focus:outline-blue-500 drop-shadow-lg tablet:w-4/6 laptop:w-2/4" placeholder="Masukkan nomor NISN anda disini" />
+                        <button className="font-lato w-9/12 h-9 m-auto mt-3 font-semibold rounded-md bg-blue-500 hover:bg-blue-600 active:bg-blue-700 duration-150 tablet:w-4/6  laptop:w-2/4">Cek NISN</button>
                     </div>
                 </form>
             </div>
